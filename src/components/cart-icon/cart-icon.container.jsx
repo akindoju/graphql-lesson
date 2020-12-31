4c10928a35a0ca14bfd91bfad9c676ab8d1fc783
Akndju <davidd.akindoju@gmail.com>
2020-12-31 15:26:55 +0100

    Used Query and Mutation to handle cart-icon, cart-dropdown, toggle cart hidden in the app

--- src/components/cart-icon/cart-icon.container.jsx
+++ src/components/cart-icon/cart-icon.container.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { Mutation } from 'react-apollo';
 import { gql } from 'apollo-boost';
 import CartIcon from './cart-icon.component';
-import { toggleCartHidden } from '../../redux/cart/cart.actions';
 
 const TOGGLE_CART_HIDDEN = gql`
   mutation ToggleCartHidden {
